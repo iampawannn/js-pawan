c9aebbe2c06a5e0bf84d4c8ce5c2e4a2d52efd00
iampawannn <pawonebhattarai@gmail.com>
2024-07-31 13:04:38 +0000

    data

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const acoountId = 144553
+let accountEmail = "pawone@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//accountId=2
+
+acoountId = 144564
+accountEmail = "dfgdf@gmail.com"
+accountPassword = "124564345"
+accountCity = "Sydney"
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*Prefer not to use var 
+because of block scope and functional scope*/
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+
+// alert(3+3) we are using nodejs, not browser
+
+console.log("Pawan");
+
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn= false
+let state;
+//number => 2 to power 53
+//bigInt
+//string=> ""
+//boolean => true/false
+//null => standalone value //representation of empty value
+//undefined => 
+//symbol => unique 
+
+//object 
+
+console.log(typeof "hitesh")
+console.log(type of undefined);
